69ec1b76373b6420db3916f353c31271720c59e3
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-25 19:11:33 +0600

    created user-info api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,27 @@ const User = require("../models/userModel");
 
 
 const userInfo = async(req,res)=>{
-    
+    try {
+        
+        const user = req.body;
+
+        const newUser = new User(user);
+
+        const data = await newUser.save();
+
+        res.json({
+            success: true,
+            message: "User logged in successfully",
+            data
+
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
 }
 
 
