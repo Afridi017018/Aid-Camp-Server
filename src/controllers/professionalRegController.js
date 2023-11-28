85d326f2cedd84be9801d12e8d5fa19bade5ad77
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-28 21:40:27 +0600

    interested professional on backend handled

--- src/controllers/professionalRegController.js
+++ src/controllers/professionalRegController.js
@@ -1,3 +1,4 @@
+const Camp = require("../models/campModel");
 const Professional = require("../models/professionalRegModel");
 
 const professionalReg = async (req, res) => {
@@ -29,4 +30,67 @@ const professionalReg = async (req, res) => {
 
 
 
-module.exports = {professionalReg}
+const getInterestedProfessionals = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        // console.log(id)
+
+
+        const data = await Professional.find({ campId: id }).populate("userId").populate("campId");
+
+        // console.log(data)
+
+        res.json({
+            success: true,
+            message: "All interested professionals !",
+            data
+
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+
+const acceptInterestedProfessionals = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+    
+
+        const info = await Professional.findByIdAndUpdate({ _id: id },{approve_status: "accepted"});
+
+
+
+        const count = await Camp.findById({ _id: info.campId.toString() });
+
+        await Camp.findByIdAndUpdate({ _id: info.campId.toString() }, { professional_count: count.professional_count + 1 })
+
+
+        res.json({
+            success: true,
+            message: "Accepted !",
+            // data
+
+        });
+
+    } catch (error) {
+        // console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+
+module.exports = {professionalReg, getInterestedProfessionals, acceptInterestedProfessionals}
--- src/routers/professionalRegRouter.js
+++ src/routers/professionalRegRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { professionalReg } = require('../controllers/professionalRegController');
+const { professionalReg, getInterestedProfessionals, acceptInterestedProfessionals } = require('../controllers/professionalRegController');
 
 const router = express.Router();
 
 
 router.post('/professional-reg', professionalReg);
+
+router.get('/get-interested-professionals/:id', getInterestedProfessionals)
+
+router.put('/accept-interested-professionals/:id', acceptInterestedProfessionals)
 
 
 
