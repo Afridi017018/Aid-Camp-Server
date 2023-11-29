b135252c83bc01f57062958b465ee30f90967de3
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-29 16:49:35 +0600

    created logout api

--- src/controllers/verificationController.js
+++ src/controllers/verificationController.js
@@ -3,17 +3,50 @@ const { jwtSecret } = require('../secret');
 
 
 const accessToken = async (req, res) => {
-    const { email } = req.body;
-    
-    const token = jwt.sign(email, jwtSecret);
 
-    res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-    }).json({ success: true });
+    try {
+        const { email } = req.body;
+
+        const token = jwt.sign(email, jwtSecret);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        }).json({ success: true });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+
+    }
 
 }
 
 
-module.exports = {accessToken}
+
+const logout = async (req, res) => {
+
+    try {
+        // console.log("User Logged out: ", req.body.user);
+        res.clearCookie('token', {
+            maxAge: 0,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+        }).json({ success: true });
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+
+}
+
+
+module.exports = { accessToken, logout }
--- src/routers/verificationRouter.js
+++ src/routers/verificationRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { accessToken } = require('../controllers/verificationController');
+const { accessToken, logout } = require('../controllers/verificationController');
 const router = express.Router();
 
 
 
 router.post("/access-token", accessToken )
+
+router.post('/logout', logout)
 
 
 
